0133a7e397600354daabdc832dd889566bb02e84
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-04-10 14:23:35 +0000

    fixing view

--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -7,13 +7,15 @@ const Recipe = props => (
 			{ props.recipes.map((recipes) => {
         	return  (
         			<div key={recipes.title} className="col-md-4" style={{marginBottom:'2rem'}}>
-        				<div className="recipe__box">
+        				<div className="recipes__box">
         					<img className='recipe__box-img' src={recipes.image_url} alt={recipes.title}/>
         					<div className="recipe__text">
-        						<h5 className="recipes__title">{recipes.title}</h5>
+        						<h5 className="recipes__title">
+        							{recipes.title.length < 20 ? `${recipes.title}` : `${recipes.title.substring(0,25)}...` }
+        						</h5>
         						<p className="recipes__subtitle">Publisher: <span>{recipes.publisher}</span></p>
         					</div>
-        					<button className="recipes__button">View Recipe</button>
+        					<button className="recipe_buttons">View Recipe</button>
         				</div>
         			</div>
         		);
