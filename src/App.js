8456c6dfd8497516bf502a00740ccb9c2928db60
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-04-10 11:31:21 +0000

    getting response from api and view it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './component/Form'
+import Recipe from './component/Recipe'
 
 const API_KEY = 'a80e9fb3cf21ba15def59da856ee4d28';
 
@@ -11,10 +12,10 @@ class App extends Component {
 	getRecipe = async (e) => {
 		const recipeName = e.target.elements.recipeName.value;
 		e.preventDefault();
-		const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken&count=5`);
+		const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`);
 
 		const data = await api_call.json();
-		this.setState({recipes: data});
+		this.setState({recipes: data.recipes});
 		console.log(this.state.recipes)
 	}
   render() {
@@ -24,6 +25,7 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
+        <Recipe recipes={this.state.recipes}/>
       </div>
     );
   }
---
+++ src/component/Recipe.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Recipe = props => (
+		<div className='container'>
+			<div className='row'>
+			{ props.recipes.map((recipes) => {
+        	return  (
+        			<div key={recipes.title} className="col-md-4" style={{marginBottom:'2rem'}}>
+        				<div className="recipe__box">
+        					<img className='recipe__box-img' src={recipes.image_url} alt={recipes.title}/>
+        					<div className="recipe__text">
+        						<h5 className="recipes__title">{recipes.title}</h5>
+        						<p className="recipes__subtitle">Publisher: <span>{recipes.publisher}</span></p>
+        					</div>
+        					<button className="recipes__button">View Recipe</button>
+        				</div>
+        			</div>
+        		);
+        } ) }
+			</div>
+		</div>
+	);
+
+export default Recipe;
