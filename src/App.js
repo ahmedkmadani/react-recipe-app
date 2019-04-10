ee0f30e020f929ff976a5a029c501dbca3224aba
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-04-10 09:25:45 +0000

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './component/Form'
 
 class App extends Component {
+	getRecipe = (e) => {
+		e.preventDefault();
+		console.log('working fine');
+	}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe}/>
       </div>
     );
   }
