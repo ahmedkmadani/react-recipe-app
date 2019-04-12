4c390fa5eb6fbc2a3c0df278e38888746f09c735
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-04-12 10:20:48 +0000

    remove Mount function and Update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from './component/Form'
-import Recipe from './component/Recipe'
 
-const API_KEY = 'a80e9fb3cf21ba15def59da856ee4d28';
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+const API_KEY = "893632c6e784af5934311ff42f22ef51";
 
 class App extends Component {
-	state ={
-		recipes: []
-	}
-	getRecipe = async (e) => {
-		const recipeName = e.target.elements.recipeName.value;
-		e.preventDefault();
-		const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=5`);
-
-		const data = await api_call.json();
-		this.setState({recipes: data.recipes});
-		console.log(this.state.recipes)
-	}
+  state = {
+    recipes: []
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  }
+  // componentDidMount = () => {
+  //   const json = localStorage.getItem("recipes");
+  //   const recipes = JSON.parse(json);
+  //   this.setState({ recipes });
+  // }
+  // componentDidUpdate = () => {
+  //   const recipes = JSON.stringify(this.state.recipes);
+  //   localStorage.setItem("recipes", recipes);
+  // }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form getRecipe={this.getRecipe}/>
-        <Recipe recipes={this.state.recipes}/>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Router from './component/Router'
+import Router from './components/Router'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
